352143e2207e3efd66d8c054e20a3793c8580671
yosiG <gamlielyosi@gmail.com>
2014-03-13 21:56:40 +0200

    interface friend

---
+++ BE/src/person/friendInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package person;
+
+/**
+ *Represents a user who is not a friend
+ * @author g
+ */
+public interface friendInterface {
+/**
+ * Make a comment of friend
+      * @param myCommant
+ */
+    void putComments(String myCommant);//להחליף את סטרינג להערה
+}
